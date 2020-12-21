659750541638e2cb6a8823e055f593b838d82acc
Dwight Biddle <dwight.biddle@ef.com>
2020-12-20 22:47:45 -0500

    Add fake measure tab

--- pages/measure/measure-input.jsx
+++ pages/measure/measure-input.jsx
@@ -1,3 +1,30 @@
-export default function PlanInput() {
-    return <h1>Choose a map</h1>
+import React from 'react'
+import Header from '../../components/header';
+import styles from '../../styles/Home.module.css'
+import Image from 'next/image'
+
+export default function MeasureInput() {
+  return (
+    <div>
+      <Header/>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>
+            Choose a sample site to measure
+          </h1>
+
+          <p className={styles.description}>
+            Ensure your device's clock is synchronized with your device
+          </p>
+          <Image
+            src="/map.png"
+            alt="Picture of the author"
+            width={400}
+            height={300}
+          />
+          <button type="button" class="btn btn-primary">Measure</button>
+        </div>
+      </main>
+    </div>
+  )
   }
